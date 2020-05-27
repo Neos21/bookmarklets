9c9b9206972b5a00a2a4a51a3bc4e30348ace34f
Neos21 <neos21@gmail.com>
2020-05-27 16:52:58 +0900

    Add 'Open gyo.tc' and Update build scripts

--- scripts/build-scripts.js
+++ scripts/build-scripts.js
@@ -7,14 +7,37 @@ const uglifyEs = require('uglify-es');
 const srcDir = './src';
 const distDir = './dist';
 
+/**
+ * JS ファイル1つをビルドする
+ * 
+ * @param {string} fileName ファイル名
+ */
+function buildFile(fileName) {
+  const uglified = uglifyEs.minify({
+    file: fs.readFileSync(path.join(srcDir, fileName), 'utf-8')
+  }).code;
+  fs.writeFileSync(path.join(distDir, fileName), uglified, 'utf-8');
+}
+
 if(!fs.existsSync(distDir)) {
   fs.mkdirSync(distDir);
 }
 
-fs.readdirSync(srcDir).forEach((fileName) => {
-  const uglified = uglifyEs.minify({
-    file: fs.readFileSync(path.join(srcDir, fileName), 'utf-8')
-  }).code;
-  
-  fs.writeFileSync(path.join(distDir, fileName), uglified, 'utf-8');
-});
+if(process.argv.length > 2) {
+  let fileName = process.argv[2];  // 0・1 は確定・2 が第1引数となる
+  if(!fileName.endsWith('.js')) {
+    fileName += '.js';
+  }
+  if(!fs.existsSync(path.join(srcDir, fileName))) {
+    console.error(`The File [${fileName}] Does Not Exist. Aborted`);
+    return;
+  }
+  console.log(`Build The File [${fileName}]`);
+  buildFile(fileName);
+}
+else {
+  console.log('Build All Files');  // 通常は全ファイルをビルドする
+  fs.readdirSync(srcDir).forEach((fileName) => {
+    buildFile(fileName);
+  });
+}
