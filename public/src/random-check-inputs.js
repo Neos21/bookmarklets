2655fd0e002cb7981bf96433591f929cf0c6420e
Neos21 <neos21@gmail.com>
2020-09-01 16:48:38 +0900

    Modify Random Check Inputs

--- public/src/random-check-inputs.js
+++ public/src/random-check-inputs.js
@@ -3,20 +3,20 @@
  * 
  * <p>ラジオボタン <code>input[type=&quot;radio&quot;]</code>・チェックボックス <code>input[type=&quot;checkbox&quot;]</code> を <code>name</code> 属性ごとに1つランダムにクリックする。</p>
  */
-((elems) => {
-  [].forEach.call(document.querySelectorAll('[type=radio],[type=checkbox]'), (elem) => {
-    if(!elem.name) {
-      return;
-    }
-    
+((doc, all, elems) => {
+  // name 属性を持つモノだけに絞り込む
+  doc[all]('[type=radio][name],[type=checkbox][name]').forEach((elem) => {
     if(elems[elem.name] === undefined) {
       elems[elem.name] = [];
     }
-    
     elems[elem.name].push(elem);
   });
   
+  // name 属性ごとに1つだけクリックする
   Object.keys(elems).forEach((name) => {
     elems[name][Math.floor(Math.random() * elems[name].length)].click();
   });
-})({});
+  
+  // 何らかのラベルをクリックした時だけボタンを押す
+  doc[all]('#finishButton,[value*="次へ"],[value*="送"]').forEach((btn) => { btn.click(); });
+})(document, 'querySelectorAll', {});
